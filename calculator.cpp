28439a06a5c375535a227caec28b10550e52a198
Donut <punnatat_ng@cmu.ac.th>
2024-02-27 16:23:16 +0700

    top secret

--- calculator.cpp
+++ calculator.cpp
@@ -1,10 +1,103 @@
 #include <windows.h>
-
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+char firstinputsave[100];
+char secondinputsave[100];
 /* This is where all the input to the window goes to */
 LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
+	
+	HWND textfield,plusbutton,minusbutton,multiplybutton,dividebutton;
+	HWND firstinput,secondinput;
 	switch(Message) {
-		
+		case WM_ERASEBKGND: {
+		HDC hdc = (HDC)wParam;
+		RECT rect;
+		GetClientRect(hwnd, &rect);
+		HBRUSH hBrush = CreateSolidBrush(RGB(210, 0, 0)); 
+		FillRect(hdc, &rect, hBrush);
+		DeleteObject(hBrush);
+		return (LRESULT)TRUE; 
+	}
+		case WM_CREATE:
+			textfield = CreateWindow("STATIC",
+									"Please input two numbers",
+									WS_VISIBLE | WS_CHILD | WS_BORDER,
+									20,20,180,20,
+									hwnd,NULL,NULL,NULL);
+			plusbutton = CreateWindow("BUTTON",
+										"+",
+										WS_VISIBLE | WS_CHILD | WS_BORDER,
+										40,120,20,20,
+										hwnd,(HMENU) 1,NULL,NULL);
+			minusbutton = CreateWindow("BUTTON",
+										"-",
+										WS_VISIBLE | WS_CHILD | WS_BORDER,
+										70,120,20,20,
+										hwnd,(HMENU) 2,NULL,NULL);
+			multiplybutton = CreateWindow("BUTTON",
+										"*",
+										WS_VISIBLE | WS_CHILD | WS_BORDER,
+										100,120,20,20,
+										hwnd,(HMENU) 3,NULL,NULL);
+			dividebutton = CreateWindow("BUTTON",
+										"/",
+										WS_VISIBLE | WS_CHILD | WS_BORDER,
+										130,120,20,20,
+										hwnd,(HMENU) 4,NULL,NULL);
+			firstinput = CreateWindow("EDIT",
+										"",
+										WS_VISIBLE | WS_CHILD | WS_BORDER,
+										20,50,150,20,
+										hwnd,NULL,NULL,NULL);
+			secondinput = CreateWindow("EDIT",
+										"",
+										WS_VISIBLE | WS_CHILD | WS_BORDER,
+										20,80,150,20,
+										hwnd,NULL,NULL,NULL);
+			break;
 		/* Upon destruction, tell the main thread to stop */
+		case WM_COMMAND:
+				
+				GetWindowText(firstinput, &firstinputsave[100], 100);
+            	GetWindowText(secondinput, &secondinputsave[100], 100);
+				double a,b;
+				 a = atof(firstinputsave);
+				 b = atof(secondinputsave);
+				char result[100];
+				double plus,minus,time,divide;
+			switch (LOWORD(wParam))
+			{
+				
+			case 1:
+				plus = a + b;
+				sprintf(result,"%.6f",plus);
+				::MessageBeep(MB_ICONERROR);
+				::MessageBox(hwnd, result ,"Result",MB_OK);
+				break;
+			case 2:
+				minus = a-b;
+				sprintf(result,"%.6f",minus);
+				::MessageBeep(MB_ICONERROR);
+				::MessageBox(hwnd, result ,"Result",MB_OK);
+				break;
+			case 3:
+				time = a*b;
+				sprintf(result,"%.6f",time);
+				::MessageBeep(MB_ICONERROR);
+				::MessageBox(hwnd,result,"Result",MB_OK);
+				break;
+			case 4:
+				divide = a/b;
+				sprintf(result,"%.6f",divide);
+				::MessageBeep(MB_ICONERROR);
+				::MessageBox(hwnd,result,"Result",MB_OK);
+				break;
+			default:
+				break;
+			}
+			break;
+
 		case WM_DESTROY: {
 			PostQuitMessage(0);
 			break;
@@ -41,11 +134,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return 0;
 	}
 
-	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","Caption",WS_VISIBLE|WS_OVERLAPPEDWINDOW,
+	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","My Calculator",WS_VISIBLE|WS_SYSMENU,
 		CW_USEDEFAULT, /* x */
 		CW_USEDEFAULT, /* y */
-		640, /* width */
-		480, /* height */
+		250, /* width */
+		200, /* height */
 		NULL,NULL,hInstance,NULL);
 
 	if(hwnd == NULL) {
